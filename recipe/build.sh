fb05cab871448c8760f9aa59ff4bfc6dc73a9224
Loïc Estève <loic.esteve@ymail.com>
2024-03-15 13:23:22 +0100

    Apply suggestions from code review
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,12 +10,16 @@ export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 # to use host python; requires that [binaries] section is last in meson_cross_file
 echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
 
+# conda-forge itself prepropulates MESON_ARGS for cross-compilation with meson:
+# https://conda-forge.org/docs/maintainer/knowledge_base/#how-to-enable-cross-compilation
 # meson-python already sets up a -Dbuildtype=release argument to meson, so
 # we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
 MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
 
 mkdir builddir
 # -wnx flags mean: --wheel --no-isolation --skip-dependency-check
+# Forward MESON_ARGS_REDUCED to our Python build tool by prepending -Csetup-args=
+# in front of every arguments in MESON_ARGS_REDUCED.
 $PYTHON -m build -w -n -x \
         -Cbuilddir=builddir \
         -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=} \
